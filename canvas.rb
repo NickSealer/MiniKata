11325bef20f899dab744bf3971e461c7e6d18785
Nick Fontecha <nickfontechav@gmail.com>
2018-12-16 11:22:06 -0500

    Codigo huge comentariado con exlicación

--- canvas.rb
+++ canvas.rb
@@ -1,22 +1,29 @@
 require './huge'
 
-puts 'Ingrese el alto del canvas'
+puts 'Ingrese la dimensiones para crear el canvas'
+puts 'Ingrese el alto del canvas:'
 h = gets.chomp.to_i
-puts 'Ingrese el ancho del canvas'
+puts 'Ingrese el ancho del canvas:'
 w = gets.chomp.to_i
 d = DrawTool.new(h, w)
 d.create_canvas
 d.show_canvas
-
-puts 'Dibujar linea horizontal (Posición, Desde, Hasta)'
-n = gets.chomp.to_i
-x1 = gets.chomp.to_i
-x2 = gets.chomp.to_i
-d.create_horizontal_line((n - 1), (x1 - 1), (x2 - 1))
-d.show_canvas
-
-d.looping_vertical_line
-d.show_canvas
-
-d.create_square
-d.show_canvas
+puts "¿Qué desea dibujar?\n1. Linea Horizontal.\n2. Linea Verical.\n3. Ractangulo."
+option = gets.chomp.to_i
+case option
+when 1
+  puts 'Dibujar linea horizontal (Posición, Desde, Hasta):'
+  n = gets.chomp.to_i
+  x1 = gets.chomp.to_i
+  x2 = gets.chomp.to_i
+  d.create_horizontal_line((n - 1), (x1 - 1), (x2 - 1))
+  d.show_canvas
+when 2
+  d.looping_vertical_line
+  d.show_canvas
+when 3
+  d.create_square
+  d.show_canvas
+else
+  puts 'No eligió nada, gracias.'
+end
--- huge.rb
+++ huge.rb
@@ -1,17 +1,22 @@
-# instancing class DrawTool
+# Instantiating class DrawTool
 class DrawTool
   attr_accessor :h, :w
 
+  # Canvas dimentions and canvas body
   def initialize(h, w)
     @width = w
     @height = h
     @canvas = []
   end
 
+  # Method to print body canvas
   def show_canvas
     puts @canvas
   end
 
+  # Method to generate canvas
+  # create an array to fill it with - according @width length
+  # the height create width_array according @height
   def create_canvas
     width_array = []
     @width.times do
@@ -23,6 +28,14 @@ class DrawTool
     end
     end
 
+  # Method to create a horizontal line
+  # according the position take the value of @canvas and convert it to integer
+  # according to the integer (@width) puts - again
+  # according x1 and x2 values starts to insert an x depending on number and drop the last position
+  # to conservate the same length from @canvas
+  # join the - to makes look identical to @canvas
+  # insert the new array into a variable
+  # now insert the value on the same position indicate above and drop the last position to onservate the length
   def create_horizontal_line(n, x1, x2)
     line_h = []
     length = @canvas[n].size.to_i
@@ -34,12 +47,19 @@ class DrawTool
       line_h.pop
       x1 += 1
     end
-
     mini_canva = line_h.join
     @canvas.insert(n, mini_canva)
     @canvas.pop
   end
 
+  # Method to create a vertical line
+  # according the position take the value of @canvas and convert it to integer
+  # according to the integer (@width) puts - again
+  # according y1 and y2 values starts to insert an x depending on number and drop the last position
+  # to conservate the same length from @canvas
+  # join the - to makes look identical to @canvas
+  # insert the new array into a variable
+  # now insert the value on the same position indicate above and drop the last position to onservate the length
   def create_vertical_line(y1, y2)
     line_v = []
     length = @canvas[y1].size.to_i
@@ -53,8 +73,10 @@ class DrawTool
     @canvas.pop
   end
 
+  # Method to looping the vertical line cause the current method create_vertical_line just take one position from @array
+  # according with n (height) is goint to repeat the create_vertical_line method
   def looping_vertical_line
-    puts 'Cree la linea vertical (Longitud, Posición, Desde)'
+    puts 'Cree la linea vertical (Longitud, Posición, Desde):'
     n = gets.chomp.to_i
     y1 = gets.chomp.to_i
     y2 = gets.chomp.to_i
@@ -64,8 +86,13 @@ class DrawTool
     end
   end
 
+  # Method to draw a square deppendig on coordinates
+  # print upper and lower side filled of x
+  # ask to user if want to an colored square or an empty one
+  # call the respective method deppendig the option
+  # iterate the right and left side by squares's h (height)
   def create_square
-    puts 'Dimensiones del rectangulo (Posición, Desde, Hasta, Altura)'
+    puts 'Dimensiones del rectangulo (Posición, Desde, Hasta, Altura):'
     n = gets.chomp.to_i
     x1 = gets.chomp.to_i
     x2 = gets.chomp.to_i
@@ -88,6 +115,13 @@ class DrawTool
     end
   end
 
+  # Method based on create_vertical_line, it do practiaclly the same job
+  # first print the border, or an initial value with a x
+  # then starts one position ahead of x and print a space till x2
+  # now print the right border with a x
+  # assign the values to mini_canva of type string
+  # then insert the string into @canvas according the position n
+  # drop the last position to conservate the original @canvas size
   def clear_square(n, x1, x2)
     internal_square = []
     length = @canvas[n].size.to_i
@@ -103,12 +137,18 @@ class DrawTool
     end
     internal_square.insert(x2, 'x')
     internal_square.pop
-
     mini_canva = internal_square.join
     @canvas.insert(n, mini_canva)
     @canvas.pop
   end
 
+  # Method based on create_vertical_line too, it do practiaclly the same job
+  # first print the border, or an initial value with a x
+  # then starts one position ahead of x and print a c till x2
+  # now print the right border with a x
+  # assign the values to mini_canva of type string
+  # then insert the string into @canvas according the position n
+  # drop the last position to conservate the original @canvas size
   def paint_square(n, x1, x2)
     internal_square = []
     length = @canvas[n].size.to_i
@@ -124,7 +164,6 @@ class DrawTool
     end
     internal_square.insert(x2, 'x')
     internal_square.pop
-
     mini_canva = internal_square.join
     @canvas.insert(n, mini_canva)
     @canvas.pop
